510abc47fef2304fe35550e90de913dc0d1056b6
Michael Drogalis <mjd3089@rit.edu>
2014-07-27 20:26:11 -0700

    Params.

---
+++ parameterized/README.md
@@ -0,0 +1,14 @@
+# flat-workflow
+
+## Usage
+
+```text
+lein run
+```
+
+## License
+
+Copyright © 2014 Michael Drogalis
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ parameterized/src/parameterized/core.clj
@@ -0,0 +1,108 @@
+(ns parameterized.core
+  (:require [clojure.core.async :refer [chan >!! <!! close!]]
+            [onyx.peer.task-lifecycle-extensions :as l-ext]
+            [onyx.plugin.core-async]
+            [onyx.api]))
+
+(def workflow {:input {:add :output}})
+
+(defn my-adder [k {:keys [n] :as segment}]
+  (assoc segment :n (+ n k)))
+
+(defmethod l-ext/inject-lifecycle-resources
+  :parameterized.core/my-adder
+  [_ {:keys [onyx.core/task-map onyx.core/fn-params] :as pipeline}]
+  (let [k (:parameterized.core/k task-map)]
+        {:onyx.core/params [k]}))
+
+(def capacity 1000)
+
+(def input-chan (chan capacity))
+
+(def output-chan (chan capacity))
+
+(defmethod l-ext/inject-lifecycle-resources :input
+  [_ _] {:core-async/in-chan input-chan})
+
+(defmethod l-ext/inject-lifecycle-resources :output
+  [_ _] {:core-async/out-chan output-chan})
+
+(def batch-size 10)
+
+(def catalog
+  [{:onyx/name :input
+    :onyx/ident :core.async/read-from-chan
+    :onyx/type :input
+    :onyx/medium :core.async
+    :onyx/consumption :concurrent
+    :onyx/batch-size batch-size
+    :onyx/doc "Reads segments from a core.async channel"}
+
+   {:onyx/name :add
+    :onyx/ident :parameterized.core/my-adder
+    :onyx/fn :parameterized.core/my-adder
+    :onyx/type :transformer
+    :onyx/consumption :concurrent
+    :onyx/batch-size batch-size
+    :parameterized.core/k 42}
+
+   {:onyx/name :output
+    :onyx/ident :core.async/write-to-chan
+    :onyx/type :output
+    :onyx/medium :core.async
+    :onyx/consumption :concurrent
+    :onyx/batch-size batch-size
+    :onyx/doc "Writes segments to a core.async channel"}])
+
+(def input-segments
+  [{:n 0}
+   {:n 1}
+   {:n 2}
+   {:n 3}
+   {:n 4}
+   {:n 5}
+   :done])
+
+(doseq [segment input-segments]
+  (>!! input-chan segment))
+
+(close! input-chan)
+
+(def id (java.util.UUID/randomUUID))
+
+(def coord-opts
+  {:hornetq/mode :vm
+   :hornetq/server? true
+   :hornetq.server/type :vm
+   :zookeeper/address "127.0.0.1:2186"
+   :zookeeper/server? true
+   :zookeeper.server/port 2186
+   :onyx/id id
+   :onyx.coordinator/revoke-delay 5000})
+
+(def peer-opts
+  {:hornetq/mode :vm
+   :zookeeper/address "127.0.0.1:2186"
+   :onyx/id id})
+
+(def conn (onyx.api/connect :memory coord-opts))
+
+(def v-peers (onyx.api/start-peers conn 1 peer-opts))
+
+(onyx.api/submit-job conn {:catalog catalog :workflow workflow})
+
+(def results
+  (doall
+   (map (fn [_] (<!! output-chan))
+        (range (count input-segments)))))
+
+(clojure.pprint/pprint results)
+
+(doseq [v-peer v-peers]
+  ((:shutdown-fn v-peer)))
+
+(onyx.api/shutdown conn)
+
+(defn -main [& args]
+  (println "Done!"))
+
