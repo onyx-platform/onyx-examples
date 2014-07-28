b04c75070625785bcabea6181d37fa5753147f44
Michael Drogalis <mjd3089@rit.edu>
2014-07-27 20:47:14 -0700

    Interface injection.

---
+++ interface-injection/src/interface_injection/core.clj
@@ -0,0 +1,115 @@
+(ns interface-injection.core
+  (:require [clojure.core.async :refer [chan >!! <!! close!]]
+            [onyx.peer.task-lifecycle-extensions :as l-ext]
+            [onyx.plugin.core-async]
+            [onyx.api]))
+
+(defprotocol ITimesTwo
+  (times-two [this n]))
+
+(deftype DevelopmentMultiplication []
+  ITimesTwo
+  (times-two [this n] (* n 2)))
+
+(deftype ProductionMultiplication []
+  ITimesTwo
+  ;; Because who doesn't ship bugs in prod?
+  (times-two [this n] (* n 3)))
+
+(def workflow {:input {:add :output}})
+
+(defn my-adder [t {:keys [n] :as segment}]
+  (assoc segment :n (times-two t n)))
+
+(def capacity 1000)
+
+(def input-chan (chan capacity))
+
+(def output-chan (chan capacity))
+
+(defmethod l-ext/inject-lifecycle-resources :input
+  [_ _] {:core-async/in-chan input-chan})
+
+(defmethod l-ext/inject-lifecycle-resources :output
+  [_ _] {:core-async/out-chan output-chan})
+
+(def batch-size 10)
+
+(def catalog
+  [{:onyx/name :input
+    :onyx/ident :core.async/read-from-chan
+    :onyx/type :input
+    :onyx/medium :core.async
+    :onyx/consumption :concurrent
+    :onyx/batch-size batch-size
+    :onyx/doc "Reads segments from a core.async channel"}
+
+   {:onyx/name :add
+    :onyx/ident :interface-injection.core/my-adder
+    :onyx/fn :interface-injection.core/my-adder
+    :onyx/type :transformer
+    :onyx/consumption :concurrent
+    :onyx/batch-size batch-size
+    :interface-injection.core/k 42}
+
+   {:onyx/name :output
+    :onyx/ident :core.async/write-to-chan
+    :onyx/type :output
+    :onyx/medium :core.async
+    :onyx/consumption :concurrent
+    :onyx/batch-size batch-size
+    :onyx/doc "Writes segments to a core.async channel"}])
+
+(def input-segments
+  [{:n 0}
+   {:n 1}
+   {:n 2}
+   {:n 3}
+   {:n 4}
+   {:n 5}
+   :done])
+
+(doseq [segment input-segments]
+  (>!! input-chan segment))
+
+(close! input-chan)
+
+(def id (java.util.UUID/randomUUID))
+
+(def coord-opts
+  {:hornetq/mode :vm
+   :hornetq/server? true
+   :hornetq.server/type :vm
+   :zookeeper/address "127.0.0.1:2186"
+   :zookeeper/server? true
+   :zookeeper.server/port 2186
+   :onyx/id id
+   :onyx.coordinator/revoke-delay 5000})
+
+(def peer-opts
+  {:hornetq/mode :vm
+   :zookeeper/address "127.0.0.1:2186"
+   :onyx/id id
+   :onyx.peer/fn-params {:add [(DevelopmentMultiplication.)]}})
+
+(def conn (onyx.api/connect :memory coord-opts))
+
+(def v-peers (onyx.api/start-peers conn 1 peer-opts))
+
+(onyx.api/submit-job conn {:catalog catalog :workflow workflow})
+
+(def results
+  (doall
+   (map (fn [_] (<!! output-chan))
+        (range (count input-segments)))))
+
+(clojure.pprint/pprint results)
+
+(doseq [v-peer v-peers]
+  ((:shutdown-fn v-peer)))
+
+(onyx.api/shutdown conn)
+
+(defn -main [& args]
+  (println "Done!"))
+
