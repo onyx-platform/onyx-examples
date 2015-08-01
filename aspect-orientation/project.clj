4c6d1730010ec359739f6300b1fe99325463bac4
Michael Drogalis <mjd3089@rit.edu>
2015-07-31 19:54:52 -0700

    Must be on Clojure 1.7.0.

--- aspect-orientation/project.clj
+++ aspect-orientation/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.onyxplatform/onyx "0.7.0-SNAPSHOT"]
                  [dire "0.5.2"]])
--- datomic-mysql-transfer/project.clj
+++ datomic-mysql-transfer/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.3.3"]
                  [com.datomic/datomic-free "0.9.5173"]
                  [org.onyxplatform/onyx "0.7.0-SNAPSHOT"]
