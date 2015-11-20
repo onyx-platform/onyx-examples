aafc69d01662fc493108e2eb008d4611e381ddda
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-21 03:54:15 +0800

    Attempt to test all examples on CircleCI

---
+++ test_projects.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+for DIR in $(ls -d */); do
+	cd $DIR
+	NSES=$(find src -name "*.clj" |sed s/src\\///|sed s/\\//\\./g|sed s/".clj$"//|sed s/"_"/"-"/g)
+
+	for n in $NSES; do
+		lein exec -ep "(require '[$n])"
+	done
+	cd ..
+done
