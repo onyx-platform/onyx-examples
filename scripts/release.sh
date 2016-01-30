8663ec1ce048815e546f8f67a1c7580dc012ed94
Michael Drogalis <mjd3089@rit.edu>
2016-01-29 20:42:55 -0800

    Adds a release script

---
+++ scripts/release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+cd "$(dirname "$0")/.."
+
+if [[ "$#" -ne 2 ]]; then
+	echo "Usage: $0 core-version release-branch"
+	echo "Example: $0 0.8.4 0.8.x"
+fi
+
+new_core_version=$1
+
+# Update to release version.
+git checkout master
+git stash
+git pull
+
+for DIR in $(ls -d */); do
+  cd $DIR
+
+  lein update-dependency org.onyxplatform/onyx $new_core_version
+
+  git add project.clj
+  cd ..
+done
+
+git commit -m "Upgrade to $new_core_version."
+git push origin master
+
+git checkout $2
+git merge master
+git push origin $2
