66524762132e04fc8d797166e571193eb2236a86
Michael Drogalis <mjd3089@rit.edu>
2016-01-31 11:07:50 -0800

    Fix core.async versions for new promise chan.

--- aspect-orientation/project.clj
+++ aspect-orientation/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [org.clojure/core.async "0.2.371"]
                  [org.onyxplatform/onyx "0.8.8"]
                  [dire "0.5.2"]]
   :plugins [[lein-update-dependency "0.1.2"]])
