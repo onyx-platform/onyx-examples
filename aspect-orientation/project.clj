2cefe47f3d5fbd23a6ac8d5ede760fa3095a217e
OnyxBot <michael.drogalis@onyxplatform.org>
2016-02-09 03:30:17 +0000

    Upgrade to 0.8.9.

--- aspect-orientation/project.clj
+++ aspect-orientation/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.371"]
-                 [org.onyxplatform/onyx "0.8.8"]
+                 [org.onyxplatform/onyx "0.8.9"]
                  [dire "0.5.2"]]
   :plugins [[lein-update-dependency "0.1.2"]])
