9641fb19790db8863425b75527f6591342a40e81
Michael Drogalis <mjd3089@rit.edu>
2016-03-15 08:05:29 -0700

    Parallelize examples runner.

--- test_projects.sh
+++ test_projects.sh
@@ -5,12 +5,20 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
+i=0
+
 for DIR in $(ls -d */); do
-	cd $DIR
+  cd $DIR
+
+  if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
+  then
 	NSES=$(find src -name "*.clj" |sed s/src\\///|sed s/\\//\\./g|sed s/".clj$"//|sed s/"_"/"-"/g)
 
 	for n in $NSES; do
 		lein exec -ep "(require '[$n])"
 	done
-	cd ..
+  fi
+  ((++i))
+
+  cd ..
 done
