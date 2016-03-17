4e293132079336636a5b851ed686c42830df2cda
OnyxBot <michael.drogalis@onyxplatform.org>
2016-03-17 16:38:46 +0000

    Update dependency org.onyxplatform/onyx to version 0.9.0-beta1.
    Automatic commit by lein-unison.

--- aspect-orientation/project.clj
+++ aspect-orientation/project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.371"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.9.0-20160317_155045-g9ea946f"]
+                 [org.onyxplatform/onyx "0.9.0-beta1"]
                  [dire "0.5.2"]]
   :plugins [[lein-update-dependency "0.1.2"]])
