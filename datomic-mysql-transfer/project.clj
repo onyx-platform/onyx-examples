126f52791961f4924f254868b6f2c28b168266d5
OnyxBot <michael.drogalis@onyxplatform.org>
2016-09-15 19:41:13 +0000

    Update dependency org.onyxplatform/onyx to version 0.9.10-beta7.
    Automatic commit by lein-unison.

--- datomic-mysql-transfer/project.clj
+++ datomic-mysql-transfer/project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/java.jdbc "0.4.2"]
                  [com.datomic/datomic-free "0.9.5173"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.9.10-20160915_182645-g11c0e3e"]
+                 [org.onyxplatform/onyx "0.9.10-beta7"]
                  [org.onyxplatform/onyx-datomic "0.9.10.0-beta1"]
                  [org.onyxplatform/onyx-sql "0.9.10.0-beta1"]
                  [environ "1.0.1"]
