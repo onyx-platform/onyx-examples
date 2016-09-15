feae151df8a46c5a29a972a4d12c67631abe2a25
OnyxBot <michael.drogalis@onyxplatform.org>
2016-09-15 19:52:54 +0000

    Upgrade to 0.9.10-beta7.

--- datomic-mysql-transfer/project.clj
+++ datomic-mysql-transfer/project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/java.jdbc "0.4.2"]
                  [com.datomic/datomic-free "0.9.5173"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.9.10-beta6"]
+                 [org.onyxplatform/onyx "0.9.10-beta7"]
                  [org.onyxplatform/onyx-datomic "0.9.10.0-beta1"]
                  [org.onyxplatform/onyx-sql "0.9.10.0-beta1"]
                  [environ "1.0.1"]
