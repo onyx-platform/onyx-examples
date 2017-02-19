4ee3af384e854691a485ea2f86e7dc1509c27358
OnyxBot <michael.drogalis@onyxplatform.org>
2017-02-19 09:38:42 +0000

    Upgrade to 0.10.0-beta3.

--- session-windows/project.clj
+++ session-windows/project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-beta2"]]
+                 [org.onyxplatform/onyx "0.10.0-beta3"]]
   :plugins [[lein-update-dependency "0.1.2"]])
