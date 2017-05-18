1c00859646728e228edab251f859863376c25f22
OnyxBot <michael.drogalis@onyxplatform.org>
2017-05-18 01:05:21 +0000

    Update dependency org.onyxplatform/onyx to version 0.10.0-beta16.
    Automatic commit by lein-unison.

--- aspect-orientation/project.clj
+++ aspect-orientation/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-20170518_002436-g9aaf69a"]
+                 [org.onyxplatform/onyx "0.10.0-beta16"]
                  [dire "0.5.2"]]
   :plugins [[lein-update-dependency "0.1.2"]])
