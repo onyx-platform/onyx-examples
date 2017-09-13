a3742a45084e2ff61db1e1446582e6c081bc4d52
OnyxBot <michael.drogalis@onyxplatform.org>
2017-09-13 20:44:13 +0000

    Upgrade to 0.11.0-alpha4.

--- parameterized/project.clj
+++ parameterized/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.0-alpha3"]]
+                 [org.onyxplatform/onyx "0.11.0-alpha4"]]
   :plugins [[lein-update-dependency "0.1.2"]]
   :main ^:skip-aot parameterized.core)
