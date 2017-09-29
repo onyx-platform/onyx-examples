11a02eb3b07b76667693c37a0c362ee8bf1113fe
OnyxBot <michael.drogalis@onyxplatform.org>
2017-09-29 02:02:44 +0000

    Update dependency org.onyxplatform/onyx to version 0.11.1-20170929_014850-g0e587cb.
    Automatic commit by lein-unison.

--- flow-predicate-composition/project.clj
+++ flow-predicate-composition/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.1-20170929_011639-g969b23c"]]
+                 [org.onyxplatform/onyx "0.11.1-20170929_014850-g0e587cb"]]
   :plugins [[lein-update-dependency "0.1.2"]]
   :main ^:skip-aot flow-predicate-composition.core)
