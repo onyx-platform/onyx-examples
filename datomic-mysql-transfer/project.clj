29d486c5b0d0fc48a4b09f2a2495a9b9514ad60b
OnyxBot <michael.drogalis@onyxplatform.org>
2017-12-04 21:29:03 +0000

    Upgrade to 0.12.0-rc2.

--- datomic-mysql-transfer/project.clj
+++ datomic-mysql-transfer/project.clj
@@ -8,7 +8,7 @@
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  ;; onyx-sql support is currently broken in 0.10.0
                  [com.datomic/datomic-free "0.9.5554"]
-                 [org.onyxplatform/onyx "0.12.0-rc1"]
+                 [org.onyxplatform/onyx "0.12.0-rc2"]
                  [org.onyxplatform/onyx-datomic "0.10.0.0-beta5"]
                  ; [org.onyxplatform/onyx-sql "0.10.0.0-alpha1"]
                  [environ "1.0.1"]
