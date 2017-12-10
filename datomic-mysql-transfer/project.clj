5599a72b325cd87e003b315b6a03b8a3b2d741e6
OnyxBot <michael.drogalis@onyxplatform.org>
2017-12-10 06:43:55 +0000

    Update dependency org.onyxplatform/onyx to version 0.12.1-20171210_063544-gab0c010.
    Automatic commit by lein-unison.

--- datomic-mysql-transfer/project.clj
+++ datomic-mysql-transfer/project.clj
@@ -8,7 +8,7 @@
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  ;; onyx-sql support is currently broken in 0.10.0
                  [com.datomic/datomic-free "0.9.5554"]
-                 [org.onyxplatform/onyx "0.12.1-20171206_010517-g12655c8"]
+                 [org.onyxplatform/onyx "0.12.1-20171210_063544-gab0c010"]
                  [org.onyxplatform/onyx-datomic "0.10.0.0-beta5"]
                  ; [org.onyxplatform/onyx-sql "0.10.0.0-alpha1"]
                  [environ "1.0.1"]
