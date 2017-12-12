d0e08ee2aa83c5ac936d36b5e64d220df819c149
OnyxBot <michael.drogalis@onyxplatform.org>
2017-12-12 19:20:26 +0000

    Update dependency org.onyxplatform/onyx to version 0.12.1-20171212_191227-g7f7a96c.
    Automatic commit by lein-unison.

--- aggregation/project.clj
+++ aggregation/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.12.1-20171210_201907-gb76735f"]]
+                 [org.onyxplatform/onyx "0.12.1-20171212_191227-g7f7a96c"]]
   :plugins [[lein-update-dependency "0.1.2"]]
   :main ^:skip-aot aggregation.core)
--- datomic-mysql-transfer/project.clj
+++ datomic-mysql-transfer/project.clj
@@ -8,7 +8,7 @@
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  ;; onyx-sql support is currently broken in 0.10.0
                  [com.datomic/datomic-free "0.9.5554"]
-                 [org.onyxplatform/onyx "0.12.1-20171210_201907-gb76735f"]
+                 [org.onyxplatform/onyx "0.12.1-20171212_191227-g7f7a96c"]
                  [org.onyxplatform/onyx-datomic "0.10.0.0-beta5"]
                  ; [org.onyxplatform/onyx-sql "0.10.0.0-alpha1"]
                  [environ "1.0.1"]
