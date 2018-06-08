c96bf1343343045a61c30c630df8cde387473499
Lucas Bradstreet <lucasbradstreet@gmail.com>
2018-06-08 15:07:09 -0700

    Update project.clj
    terminal/reduce task description

--- terminal-reduce-task/project.clj
+++ terminal-reduce-task/project.clj
@@ -1,5 +1,5 @@
 (defproject terminal-reduce-task "0.1.0-SNAPSHOT"
-  :description "Demonstrates how to use a reduce task as a terminal node in a workflow."
+  :description "Demonstrates how to use a reduce task as a terminal/output node in a workflow."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
