f19a024ab78b3e15cac705089431cee7aa3efe4e
OnyxBot <michael.drogalis@onyxplatform.org>
2018-08-06 00:14:02 +0000

    Upgrade to 0.13.3-alpha4.

--- aspect-orientation/project.clj
+++ aspect-orientation/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.13.3-alpha2"]
+                 [org.onyxplatform/onyx "0.13.3-alpha4"]
                  [dire "0.5.2"]]
   :plugins [[lein-update-dependency "0.1.2"]]
   :main ^:skip-aot aspect-orientation.core)
