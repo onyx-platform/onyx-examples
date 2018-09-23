5b89d67360b815101cc03310573c42d60e050811
OnyxBot <michael.drogalis@onyxplatform.org>
2018-09-23 19:25:04 +0000

    Upgrade to 0.13.3.

--- global-windows/project.clj
+++ global-windows/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.13.3-alpha4"]]
+                 [org.onyxplatform/onyx "0.13.3"]]
   :plugins [[lein-update-dependency "0.1.2"]]
   :main ^:skip-aot global-windows.core)
